3b30b28158cd5d5d722f55553432690eaed6568c
Shannon Moeller <me@shannonmoeller.com>
2020-11-22 22:17:33 +0000

    ui updates and added saturn picture

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,3 +7,12 @@ import './enc-credits.js';
 import './enc-game.js';
 import './enc-menu.js';
 import './enc-options.js';
+
+console.log(`
+# Welcome Traveler!
+
+Looking to cheat? Neat. Try: game.goTo(3600);
+
+https://github.com/shannonmoeller/enceladus
+
+`);
